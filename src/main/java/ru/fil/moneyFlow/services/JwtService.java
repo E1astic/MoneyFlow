2aeb6657d927b0c29ccb75d2841af4090d4166f9
Filimonov Roman <darkr2004@mailru>
2025-02-26 21:56:44 +0300

    Beginning of JwtAuthenticationFilter and JwtService implementation

---
+++ src/main/java/ru/fil/moneyFlow/services/JwtService.java
@@ -0,0 +1,11 @@
+package ru.fil.moneyFlow.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class JwtService {
+
+    public String extractEmail(String jwtToken) {
+        return null;
+    }
+}
